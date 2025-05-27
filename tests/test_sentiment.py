6b92b43689bb8c96fe615c78774aecc38459eaef
kit <v1taliy-yak@yandex.ru>
2025-05-27 13:33:35 +0500

    Исправил ошибки flake8: порядок импорта и отступы

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,7 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from sentiment import predict_sentiment
+
 
 def test_sentiment_on_russian_text():
     assert predict_sentiment("Это отлично!") in ["радость"]
